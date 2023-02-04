6b4c90cf2f6f8256349395173d1bc9759aa1dc5a
tianea2160 <rhlehfndvkd7557@daum.net>
2023-02-04 16:06:23 +0900

    fix : 예외 상황 발생시 body의 status가 http header status에 적용되지 않는 문제 해결

--- src/main/java/com/mate/carpool/shared/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/mate/carpool/shared/exception/handler/GlobalExceptionHandler.java
@@ -2,8 +2,8 @@ package com.mate.carpool.shared.exception.handler;
 
 import com.mate.carpool.shared.dto.ErrorResponse;
 import com.mate.carpool.shared.exception.CustomHttpException;
-import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -12,13 +12,17 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(CustomHttpException.class)
-    public ErrorResponse customHttpException(CustomHttpException exception) {
-        return new ErrorResponse(exception.getStatus(), exception.getMessage());
+    public ResponseEntity<ErrorResponse> customHttpException(CustomHttpException exception) {
+        return ResponseEntity
+                .status(exception.getStatus())
+                .body(new ErrorResponse(exception.getStatus(), exception.getMessage()));
     }
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ErrorResponse methodArgumentNotValidException(MethodArgumentNotValidException exception) {
-        return new ErrorResponse(HttpStatus.BAD_REQUEST,exception.getMessage());
+    public ResponseEntity<ErrorResponse> methodArgumentNotValidException(MethodArgumentNotValidException exception) {
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(new ErrorResponse(HttpStatus.BAD_REQUEST, exception.getMessage()));
     }
 }
 
